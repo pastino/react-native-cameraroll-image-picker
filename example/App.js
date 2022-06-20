c166eeecb4d25e192c41f0ee883940e835d5ed99
taejun <joon5006@naver.com>
2022-06-20 18:06:04 +0900

    fix: edit README file

--- example/App.js
+++ example/App.js
@@ -1,26 +1,60 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, ScrollView, View} from 'react-native';
 import ImagePicker, {getAlbums} from 'react-native-cameraroll-image-picker';
+import Header from './src/Header';
+import usePhotoLibraryPermission from './src/PhotoLibraryPermission';
 
-const App = () => {
+const ImagePickerView = () => {
+  const [albums, setAlbums] = useState([{label: 'All', value: 'All'}]);
   const [currentAlbum, setCurrentAlbum] = useState('All');
-  // useEffect(() => {
-  //   (async function handleGetAlbums() {
-  //     const result = await getAlbums();
-  //     setAlbums(result);
-  //   })();
-  // }, []);
+  const [selected, setSelected] = useState([]);
+
+  const {getAvailability} = usePhotoLibraryPermission();
+
+  const handleSelectAlbum = album => {
+    setCurrentAlbum(album);
+  };
+
+  const handleCompleteSelect = async () => {
+    if (!selected?.[0]?.uri) return;
+    console.log(selected);
+  };
+
+  const getLibraryPermission = async () => {
+    await getAvailability();
+  };
+
+  const getAlbumHandler = async () => {
+    const result = await getAlbums();
+    setAlbums(result);
+  };
+
+  useEffect(() => {
+    getLibraryPermission();
+    getAlbumHandler();
+  }, []);
 
   return (
-    <View style={{flex: 1}}>
-      <ImagePicker
-        initialNumToRender={50}
-        album={currentAlbum}
-        maximum={5}
-        onChangePhotosEvent={e => setSelected(e?.selected)}
-      />
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <Header
+          onSelectAlbum={handleSelectAlbum}
+          onCompleteSelect={handleCompleteSelect}
+          albums={albums}
+          currentAlbum={currentAlbum}
+        />
+        <ScrollView>
+          <ImagePicker
+            initialNumToRender={50}
+            album={currentAlbum}
+            maximum={10}
+            onChangePhotosEvent={e => setSelected(e?.selected)}
+            isMultiSelect={true}
+          />
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default App;
+export default ImagePickerView;
